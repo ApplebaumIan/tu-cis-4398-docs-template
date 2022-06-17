d7893b9911a5482071c17d98644a91ce8363425a
Ian Applebaum <Ian.tyler@electricbluefish.com>
2022-06-17 14:03:04 -0400

    Initial commit

---
+++ docusaurus.config.js
@@ -0,0 +1,164 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'CIS 4398 Documentation Template',
+  tagline: 'Owls are cool',
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'facebook', // Usually your GitHub org/user name.
+  projectName: 'docusaurus', // Usually your repo name.
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          routeBasePath: 'docs',
+          path: 'docs',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        // tutorials: {
+        //   sidebarPath: require.resolve('./tutorialSidebars.js'),
+        //   // Please change this to your repo.
+        //   // Remove this to remove the "edit this page" links.
+        //   editUrl:
+        //     'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        // },
+
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+    [
+      'redocusaurus',
+      {
+        // Plugin Options for loading OpenAPI files
+        specs: [
+          {
+            id: 'using-single-yaml',
+            spec: 'static/openapi.yml.yaml',
+            route: '/api/',
+          },
+        ],
+        // Theme Options for modifying how redoc renders them
+        theme: {
+          // Change with your site colors
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+  plugins: [
+    [
+      '@docusaurus/plugin-content-docs',
+      {
+        id: 'tutorial',
+        path: 'tutorial',
+        routeBasePath: 'tutorial',
+        sidebarPath: require.resolve('./sidebars.js'),
+        // ... other options
+      },
+    ],
+  ],
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'My Site',
+        logo: {
+          alt: 'My Site Logo',
+          src: 'https://upload.wikimedia.org/wikipedia/commons/1/17/Temple_T_logo.svg',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: 'Documentation',
+          },{
+            to: '/tutorial/Intro',
+            label: 'Docusaurus Tutorial',
+            position: 'left',
+            activeBaseRegex: `/tutorial/`,
+          },
+          {
+            href: 'https://github.com/facebook/docusaurus',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Documentation',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Stack Overflow',
+                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+              },
+              {
+                label: 'Discord',
+                href: 'https://discordapp.com/invite/docusaurus',
+              },
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/docusaurus',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'GitHub',
+                href: 'https://github.com/facebook/docusaurus',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
---
+++ static/mvc-calc/html/search/functions_1.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['calculatorcontroller_0',['CalculatorController',['../classedu_1_1temple_1_1cis3296_1_1_calculator_controller.html#adf72b820f363ff5e743ad7c74c41bb09',1,'edu::temple::cis3296::CalculatorController']]],
+  ['calculatormodel_1',['CalculatorModel',['../classedu_1_1temple_1_1cis3296_1_1_calculator_model.html#a7e05bee72dfd1c67f6549a805a07d355',1,'edu::temple::cis3296::CalculatorModel']]],
+  ['calculatorviewcli_2',['CalculatorViewCli',['../classedu_1_1temple_1_1cis3296_1_1_calculator_view_cli.html#a230d97166d71a39975d71ce9926cc62c',1,'edu::temple::cis3296::CalculatorViewCli']]],
+  ['calculatorviewgui_3',['CalculatorViewGui',['../classedu_1_1temple_1_1cis3296_1_1_calculator_view_gui.html#aaf292446d6d42f9c79641f93e2095b6d',1,'edu::temple::cis3296::CalculatorViewGui']]]
+];
