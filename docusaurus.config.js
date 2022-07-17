be246fdaa7a1cd480cc50656e789dab56a566bbe
Ian Applebaum <Ian.tyler@electricbluefish.com>
2022-07-17 17:33:54 -0400

    Added edit link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -40,7 +40,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/'+process.env.ORG_NAME+'/'+process.env.PROJECT_NAME+'/edit/main/',
         },
         // tutorials: {
         //   sidebarPath: require.resolve('./tutorialSidebars.js'),
