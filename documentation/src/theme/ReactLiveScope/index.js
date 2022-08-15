5ac0b64002264f0fe9a85e3c5395cfc5faec14c3
Ian Applebaum <Ian.tyler@electricbluefish.com>
2022-08-15 13:10:58 -0400

    interactive live editor needs scope changes.

---
+++ documentation/src/theme/ReactLiveScope/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Figure from "../../components/Figure";
+import dino from "/static/img/docusaurus.png"
+// Add react-live imports you need here
+const ReactLiveScope = {
+  React,
+  ...React,
+  Figure,
+  dino
+
+};
+export default ReactLiveScope;
