919df3f365361133a0a5d279809e0a77eb282915
Ian Applebaum <Ian.tyler@electricbluefish.com>
2022-08-15 16:18:25 -0400

    Changed dino to dinosaur.

--- documentation/src/theme/ReactLiveScope/index.js
+++ documentation/src/theme/ReactLiveScope/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Figure from "../../components/Figure";
-import dino from "/static/img/docusaurus.png"
+import dinosaur from "/static/img/docusaurus.png"
 // Add react-live imports you need here
 const ReactLiveScope = {
   React,
   ...React,
   Figure,
-  dino
+  dinosaur
 
 };
 export default ReactLiveScope;
