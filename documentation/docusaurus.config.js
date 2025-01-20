336a9309bd451b58dc85c666d24339ddb94fcc38
Ian Tyler Applebaum <9451941+ApplebaumIan@users.noreply.github.com>
2025-01-20 06:50:24 +0000

    fix need for PROJECT_NAME

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -19,10 +19,21 @@ const main_template_jira_scripts = () => {
   }
 }
 
-// You can change the title here. The default is the name of the repository.
-const title = ''+process.env.PROJECT_NAME.replaceAll('-',' ').split(' ').map((word) => {
-  return word[0].toUpperCase() + word.substring(1);
-}).join(' ');
+// Fallback value if PROJECT_NAME is not defined:
+const rawProjectName = process.env.PROJECT_NAME || 'docs-dev-mode';
+
+// Transform PROJECT_NAME (or fallback) to a title-like string:
+const title = rawProjectName
+  .replaceAll('-', ' ')
+  .split(' ')
+  .map(word => {
+    // Make sure the word has at least one character
+    return word.length > 0 
+      ? word[0].toUpperCase() + word.substring(1) 
+      : '';
+  })
+  .join(' ');
+
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
