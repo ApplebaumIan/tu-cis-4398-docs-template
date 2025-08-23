c6fecd215ddd958b764b8e6938140491893dc4c6
Ian Applebaum <Ian.tyler@electricbluefish.com>
2025-08-23 17:51:38 -0400

    Add fallback for contributors component when env variables aren't set. Also added a page on how to set env variables.

--- documentation/src/pages/index.js
+++ documentation/src/pages/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
@@ -29,10 +29,29 @@ function HomepageHeader() {
 
 
 function Contributors() {
+    const [imageError, setImageError] = useState(false);
+
+    const handleImageError = () => {
+        setImageError(true);
+    };
+
+    const contributorsImageSrc = imageError
+        ? 'https://via.placeholder.com/400x100/f0f0f0/666666?text=Contributors+Not+Available'
+        : `https://contrib.rocks/image?repo=${process.env.ORG_NAME}/${process.env.PROJECT_NAME}`;
+
+    const linkHref = imageError
+        ? 'tutorial/tutorial-basics/set-environment-variables'
+        : `https://github.com/${process.env.ORG_NAME}/${process.env.PROJECT_NAME}/graphs/contributors`;
+
     return <div className={styles.contributors}>
-        <a href={`https://github.com/${process.env.ORG_NAME}/${process.env.PROJECT_NAME}/graphs/contributors`}>
-            <img src={`https://contrib.rocks/image?repo=${process.env.ORG_NAME}/${process.env.PROJECT_NAME}`}/></a>
-            <p>Made with<a href={"https://contrib.rocks"}> contrib.rocks</a>.
+        <a href={linkHref}>
+            <img
+                src={contributorsImageSrc}
+                alt={imageError ? 'Contributors image not available - click to learn how to set environment variables.' : 'Contributors'}
+                onError={handleImageError}
+            />
+        </a>
+        <p>Made with<a href={"https://contrib.rocks"}> contrib.rocks</a>.
         </p>
     </div>;
 }
