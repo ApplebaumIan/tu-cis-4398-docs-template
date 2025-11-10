86c04b816dab4e4c8de89090e74622368bea51ce
Ian Applebaum <Ian.tyler@electricbluefish.com>
2025-11-09 19:28:14 -0500

    Don't modify the content

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -245,20 +245,6 @@ const config = {
         sourceBaseUrl: 'https://applebaumian.github.io/tu-cis-4398-docs-template/',
         outDir: 'tutorial',
         documents: ['open-source-usage.mdx'],
-        modifyContent(filename, content) {
-          if (filename.includes('open-source-usage')) {
-            const newContent = content.substring(content.indexOf('---', content.indexOf('---') + 1) + 3);
-            return {
-              filename: 'open-source-usage.mdx',
-              content: `---
-id: open-source-usage
-title: Open Source Usage
----
-${newContent}`,
-            };
-          }
-          return undefined;
-        },
       },
     ]] : []),
   ],
